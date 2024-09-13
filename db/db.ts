deb211a0672ed37b91c2c5d322d0af680a35cebc
RonnyFernandez <120387240+anonnerdfrenzy@users.noreply.github.com>
2024-09-12 17:30:47 -0700

    fake commit

--- db/db.ts
+++ db/db.ts
@@ -4,3 +4,4 @@ Airtable.configure({
   apiKey: process.env.ALEX_VAEL_AIRTABLE_API_KEY, //You'll need to add a prefix to this key for every schedule you want to make
 });
 export const base = Airtable.base(process.env.ALEX_VAEL_AIRTABLE_BASE_ID); //You'll need to add a prefix to the base id key for every schedule you want to make
+//fake comment
