147cb9b0c105f31a4adf02903ee412f58be13fd8
RonnyFernandez <120387240+anonnerdfrenzy@users.noreply.github.com>
2024-09-12 17:47:01 -0700

    constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -7,15 +7,17 @@ export type NavItem = {
 };
 
 export const CONSTS = {
-  TITLE: "Example Conference Weekend",
+  TITLE: "Eternal September",
   DESCRIPTION:
-    "An Example Conference is happening from August 23 - 25, 2024 at Lighthaven. Check out the schedules for the main event and afterparty below!",
+    "Because we need money.",
   MULTIPLE_EVENTS: true,
   // If you have multiple events, add your events to the nav bar below
   // If you only have one event, you can leave the array empty
   // Find available icons at https://heroicons.com/
   NAV_ITEMS: [
-    { name: "Conference", href: "/Conference", icon: UserGroupIcon },
-    { name: "After Party", href: "/After-Party", icon: CakeIcon },
+    { name: "Week 1", href: "/week-1", icon: UserGroupIcon },
+    { name: "Week 2", href: "/week-2", icon: CakeIcon },
+    { name: "Week 3", href: "/week-3", icon: CakeIcon },
+    { name: "Week 4", href: "/week-4", icon: CakeIcon }
   ] as NavItem[],
 };
