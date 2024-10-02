2bfc3f75d7b690a9877be2484d295bf136c8c817
Habryka <panisnecis@gmail.com>
2024-10-02 15:41:14 -0700

    Revert changes we don't want from progress forum

--- utils/constants.ts
+++ utils/constants.ts
@@ -7,9 +7,9 @@ export type NavItem = {
 };
 
 export const CONSTS = {
-  TITLE: "Progress Conference 2024 - Hosted by the Roots of Progress Institute",
+  TITLE: "Example Conference Weekend",
   DESCRIPTION:
-    "A two-day event to connect people & ideas in the progress movement • October 18-19, 2024  •  Berkeley, California",
+    "An Example Conference is happening from August 23 - 25, 2024 at Lighthaven. Check out the schedules for the main event and afterparty below!",
   MULTIPLE_EVENTS: false,
   // If you have multiple events, add your events to the nav bar below
   // If you only have one event, you can leave the array empty
