57ce3ef23bc4833d0dc0adde1eac8c3167c944e4
Rafe Kennedy <dear.rafe@gmail.com>
2024-11-01 09:44:54 -0700

    Fix guests with missing names

--- db/guests.ts
+++ db/guests.ts
@@ -6,39 +6,33 @@ export type Guest = {
   Email: string;
   ID: string;
 };
-export async function getGuests() {
+
+async function getGuestsHelper(eventName?: string) {
   const guests: Guest[] = [];
+  const filterByFormulaRecord = eventName !== undefined ?
+    {
+      filterByFormula: CONSTS.MULTIPLE_EVENTS
+        ? `SEARCH("${eventName}", {Events}) != 0`
+        : "1",
+    }
+    : {};
   await base("Guests")
     .select({
       fields: ["Name", "Email"],
+      ...filterByFormulaRecord,
     })
     .eachPage(function page(records: any, fetchNextPage: any) {
+      const baseGuest = { Name: "", Email: "" };
       records.forEach(function (record: any) {
-        guests.push({ ...record.fields, ID: record.id });
+        guests.push({ ...baseGuest, ...record.fields, ID: record.id });
       });
       fetchNextPage();
     });
   return guests;
 }
 
-export async function getGuestsByEvent(eventName: string) {
-  const guests: Guest[] = [];
-  const filterFormula = CONSTS.MULTIPLE_EVENTS
-    ? `SEARCH("${eventName}", {Events}) != 0`
-    : "1";
-  await base("Guests")
-    .select({
-      fields: ["Name", "Email"],
-      filterByFormula: filterFormula,
-    })
-    .eachPage(function page(records: any, fetchNextPage: any) {
-      records.forEach(function (record: any) {
-        const fields = Object.fromEntries(
-          Object.entries(record.fields).map(([key, value]) => [key, value ?? ""])
-        ) as Omit<Guest, "ID">;
-        guests.push({ ...fields, ID: record.id });
-      });
-      fetchNextPage();
-    });
-  return guests;
-}
+
+
+export const getGuests: () => Promise<Guest[]> = getGuestsHelper;
+
+export const getGuestsByEvent: (eventName: string) => Promise<Guest[]> = getGuestsHelper;
